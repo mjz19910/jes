43ac07f0b2894e18f4ba56fee7dc4a12d101d9e0
mjz19910 <matthias291999@gmail.com>
2025-04-28 22:40:15 -0700

    move py to pyx

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from Cython.Build import cythonize
 
 
 extensions = [
-	Extension("jes", ["jes.py"]),
-	Extension("jes_sim", ["jes_sim.py"]),
-	Extension("jes_creature", ["jes_creature.py"]),
-	Extension("jes_species_info", ["jes_species_info.py"]),
-	Extension("jes_dataviz", ["jes_dataviz.py"]),
-	Extension("jes_ui", ["jes_ui.py"]),
-	Extension("jes_shapes", ["jes_shapes.py"]),
-	Extension("jes_slider", ["jes_slider.py"]),
-	Extension("jes_button", ["jes_button.py"]),
+    Extension("jes_init", ["jes_init.pyx"]),
+    Extension("jes_sim", ["jes_sim.pyx"]),
+    Extension("jes_creature", ["jes_creature.pyx"]),
+    Extension("jes_species_info", ["jes_species_info.pyx"]),
+    Extension("jes_dataviz", ["jes_dataviz.pyx"]),
+    Extension("jes_ui", ["jes_ui.pyx"]),
+    Extension("jes_shapes", ["jes_shapes.pyx"]),
+    Extension("jes_slider", ["jes_slider.pyx"]),
+    Extension("jes_button", ["jes_button.pyx"]),
 ]
 
 setup(
-    ext_modules=cythonize(extensions),
+    ext_modules=cythonize(extensions, language_level = "3"),
 )
